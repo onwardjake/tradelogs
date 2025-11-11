879d6176f8018ef9abb4ba17aac55495734bbef7
onwardjake <onwardjake@gmail.com>
2025-11-11 15:32:55 +0900

    main에 data source 사용하지 않도록 설정

--- src/main/java/com/jake/tradelogs/TradelogsApplication.java
+++ src/main/java/com/jake/tradelogs/TradelogsApplication.java
@@ -2,8 +2,10 @@ package com.jake.tradelogs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TradelogsApplication {
 
     public static void main(String[] args) {
