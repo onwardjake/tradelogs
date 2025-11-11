8e812e546dee20757b961816b2dd9f95ea470b7c
onwardjake <onwardjake@gmail.com>
2025-11-11 15:23:33 +0900

    initial commit

---
+++ src/test/java/com/jake/tradelogs/TradelogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.tradelogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradelogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
