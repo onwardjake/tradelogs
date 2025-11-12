00363fc606c76f65e94ceb1356c28203c1f3501e
onwardjake <onwardjake@gmail.com>
2025-11-12 17:22:32 +0900

    자동으로 모든 법정동코드를 가지고 오는 코드 완료

--- src/main/java/com/jake/tradelogs/controller/LawdIngestController.java
+++ src/main/java/com/jake/tradelogs/controller/LawdIngestController.java
@@ -12,6 +12,14 @@ import org.springframework.web.bind.annotation.*;
 public class LawdIngestController {
     private final LawdIngestService lawdIngestService;
 
+    @GetMapping("/all")
+    public String ingestLawdAll(){
+        // open api에서 모든 데이터를 받아와서 DB에 적재한다.
+        lawdIngestService.ingestLawdAll();
+
+        return "법정동 코드 DB 적재가 완료되었습니다.";
+    }
+
     //@PostMapping
     @GetMapping
     public String ingestLawdPages(
--- src/main/java/com/jake/tradelogs/service/LawdIngestService.java
+++ src/main/java/com/jake/tradelogs/service/LawdIngestService.java
@@ -21,7 +21,25 @@ public class LawdIngestService {
     private final LawdMapper lawdMapper;
 
     @Transactional
-    public void ingestLawdPages(Integer from, Integer to, Integer numOfRows) {
+    public void ingestLawdAll() {
+        try{
+            Integer page = 205;
+            Integer numOfRows = 100;
+            while(true){
+                // 한 페이지씩 데이터를 불러서 DB에 계속 적재한다.
+                // 읽은 데이터가 없다면 loop를 종료한다.
+                if(!ingestLawdPages(page, page, numOfRows))
+                    break;
+                page++;
+
+            }
+        } catch (Exception e) {
+            log.debug(">> [LawdIngestService::ingestLawdAll] Exception: {}", e.getMessage());
+        }
+    }
+
+    @Transactional
+    public boolean ingestLawdPages(Integer from, Integer to, Integer numOfRows) {
         List<ApiResLawdRows> rows = apiClientLawd.getPagesLawd(from, to, numOfRows);
 
         // API 응답 DTO (ApiResLawdRows) → DB 저장용 DTO(LawdCode)
@@ -29,6 +47,10 @@ public class LawdIngestService {
                 .map(LawdMapperUtil::map)
                 .filter(Objects::nonNull)
                 .toList();
+
+        if(items.isEmpty()){
+            return false;
+        }
 
         // 1000 건씩 분할해서 DB에 upsert 한다.
         int chunk = 1000;
@@ -38,5 +60,8 @@ public class LawdIngestService {
             int affected = lawdMapper.bulkUpsert(sub);
             log.info(">> [법정동코드] Upserted {} rows ({}~{} of {}, 시작페이지={}, 종료페이지={})", affected, i+1, end, items.size(), from, to);
         }
+
+        return true;
     }
+
 }
