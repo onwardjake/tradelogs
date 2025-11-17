8d5b94b54ccb1bb47b8f812f1a14b26d4d2db011
onwardjake <onwardjake@gmail.com>
2025-11-17 17:23:46 +0900

    아파트 실거래가 조회하여 화면에 출력 기능까지 완료(thymeleaf)

--- src/main/java/com/jake/tradelogs/controller/AptController.java
+++ src/main/java/com/jake/tradelogs/controller/AptController.java
@@ -26,13 +26,13 @@ public class AptController {
             @RequestParam(required = false) String sggCd,
 
             // 페이지네이션 기본값
-            @RequestParam(defaultValue = "0") int page, // 페이지 번호
+            @RequestParam(defaultValue = "1") int page, // 페이지 번호
             @RequestParam(defaultValue = "20") int size, // 페이지당 항목 개수
             @RequestParam(defaultValue = "dealDate") String sort,
             @RequestParam(defaultValue = "desc") String dir,
             Model model) {
 
-        int offset = page * size;
+        int offset = (page-1) * size;
 
         // 검색 조건 파라미터 Map 생성
         var params = new HashMap<String, Object>();
@@ -42,10 +42,18 @@ public class AptController {
         params.put("sort", sort);
         params.put("dir", dir);
 
-        // 페이지를 구성하기 위해 개수를 조회한다.
+        // 페이지를 구성하기 위해 총 데이터 개수를 조회한다.
         long total = aptService.getCount(params);
         // DB에서 데이터를 읽는다.
         List<AptTradeItem> list = aptService.getAptTradeItems(params);
+
+        // Thymeleaf 모델 전달
+        model.addAttribute("list", list);
+        model.addAttribute("total", total);
+        model.addAttribute("page", page);
+        model.addAttribute("size", size);
+        model.addAttribute("totalPages", (int)Math.ceil((double)total / size));
+        model.addAttribute("param", params);
 
         return "apttrade_list";
     }
